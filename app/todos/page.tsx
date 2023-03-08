aba0eeedd956200b583ccd4b009657ae6a62fbd6
Kevin Tatooles <tatooles@wisc.edu>
2023-03-07 22:07:05 -0600

    Implement displaying todos from the database

--- app/todos/page.tsx
+++ app/todos/page.tsx
@@ -1,7 +1,20 @@
+async function getTodos() {
+  const res = await fetch(
+    "http://127.0.0.1:8090/api/collections/todos/records"
+  );
+  const data = await res.json();
+  return data?.items as any[];
+}
+
 export default async function TodoPage() {
+  const todos = await getTodos();
+
   return (
     <div>
       <h1>Here are your todos!</h1>
+      {todos?.map((todo) => {
+        return <div key={todo.id}>{todo.title}</div>;
+      })}
     </div>
   );
 }
